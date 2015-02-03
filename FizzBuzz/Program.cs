5919446a52829ff0c2da0fba80d2b1326fae67f3
Paul Hennessey <paul@paulhennessey.co.uk>
2015-02-03 10:54:39 +0000

    First commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+
+        }
+
+        private void Run()
+        {
+            var list = Enumerable.Range(1, 100);
+
+            foreach (var num in list)
+            {
+                if (IsMod(num, i => i % 3 == 0 && i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (IsMod(num, i => i % 3 == 0))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (IsMod(num, i => i % 5 == 0))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private bool IsMod(int i, Func<int, bool> predicate)
+        {
+            return predicate(i);
+        }
+
+
+    }
+}
